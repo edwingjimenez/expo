4cfe8cb222a2437c71095a52588aeece63a3939d
edwingjimenez <jimenezedwing20@gmail.com>
2025-09-10 18:52:11 +0000

    cambios

--- guido/chat-bot/app.py
+++ guido/chat-bot/app.py
@@ -6,7 +6,7 @@ import os
 import re
 from dotenv import load_dotenv
 from googletrans import Translator
-from openai import OpenAI
+from openAi import Openai
 
 # Cargar variables de entorno desde .env
 load_dotenv()
